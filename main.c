86f732dba811fcde9e0b8786ddd3cd1e52c402ee
Michael Bishop <clever@earthtools.ca>
2013-04-03 04:51:24 +0100

    various changes, and removing dryer code

--- main.c
+++ main.c
@@ -18,7 +18,6 @@
  * PORTD:
  * PD0 == RXD
  * PD1 == TXD
- * PD2 == dryer on??
  * */
 
 #define NO_PARASITE
@@ -45,12 +44,8 @@ volatile enum my_flags state = rescan; // 16 bit, fix it to be 8 bit
 volatile uint8_t OK; // make this into a bit-field
 xbee_packet isr_packet;
 
-volatile uint8_t dryer_on = 0;
 
-ISR(INT0_vect) {
-	dryer_on = 1;
-	EIMSK &= ~_BV(INT0); // mask interupt out so it doesnt swamp the uC
-}
+//ISR(INT0_vect) {}
 
 
 EMPTY_INTERRUPT(WDT_vect);
@@ -207,9 +202,8 @@ void init() {
 	send_string_P(PSTR("entered api1 mode\n"));
 
 	DDRB |= _BV(PB1); // zone 1 control is output
-	DDRD &= ~_BV(PD2); // dryer on is input with pullup
-	PORTD |= _BV(PD2); // negative going half should yank the pullup to gnd
-	EIMSK = _BV(INT0);
+	EICRA = _BV(ISC01) | _BV(ISC00);
+	//EIMSK = _BV(INT0);
 
 	set_zone1(0); // zone 1 off by default
 #ifdef ignore_this_code
@@ -300,7 +294,6 @@ int main(void) {
 
 	// repeat forever
 	sei();
-	uint8_t dryer_status = 0;
 	int ret;
 	while (1) {
 		if (__bss_end != startups) {
@@ -309,7 +302,7 @@ int main(void) {
 				_delay_ms(1000);
 			}
 		}
-		cli();
+/*		cli();
 		if (ser_buf_size) {
 			start_tx_packet(&main_packet,0,0);
 			packet_append_string(&main_packet,"unknown bytes in serial buffer ");
@@ -323,22 +316,15 @@ int main(void) {
 			sei();
 			packet_end_send(&main_packet);
 		}
-		sei();
-		EIMSK = _BV(INT0); // turn the interupt on once per run
+		sei();*/
 		if (state & rescan) nSensors = scan_sensors();
 		if (state & dump) do_dump_sensors();
-		ret = check_temps();
+		ret = check_temps(); // +760ms
 
-		if ((dryer_status == 0) && (dryer_on == 1)) { // it just came on
-			printf("dryer came on\n");
-			dryer_status = 1;
-		} else if ((dryer_status == 1) && (dryer_on == 0)) { // it just went off
-			printf("dryer turned off\n");
-			dryer_status = 0;
-		}
-		dryer_on = 0;
+		//EIMSK = _BV(INT0); // turn the interupt on once per run
 		//if (ret != 1) XBEE_OFF;
-		wdt_delay();
+		wdt_delay(); // +8s
+		//_delay_ms(100);
 	}
 }
 int check_temps() {
