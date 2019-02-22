aa5a87c73ac22edc047d15b1213f3861bf65aa8d
Michael Bishop <cleverca22@gmail.com>
2019-02-21 22:31:42 -0400

    pin the nixpkgs

--- default.nix
+++ default.nix
@@ -1,5 +1,6 @@
+{ nixpkgs ? builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/be445a9074f.tar.gz" }:
 let
-  pkgs = import <nixpkgs> {};
+  pkgs = import nixpkgs {};
 in pkgs.pkgsCross.avr.stdenv.mkDerivation {
   name = "firmware";
   src = ./.;
